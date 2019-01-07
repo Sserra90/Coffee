a0430446b8bdb064c730e612a6b86cecb2d386d0
Sérgio Serra <sergioserra99@gmail.com>
2019-01-07 21:25:12 +0000

    Add methods for navigate back and navigate up to Coffee pages.

--- coffee/src/main/java/com/sserra/coffee/coffeepages/Page.kt
+++ coffee/src/main/java/com/sserra/coffee/coffeepages/Page.kt
@@ -1,9 +1,21 @@
 package com.sserra.coffee.coffeepages
 
-import com.sserra.coffee.changeToLandscape
-import com.sserra.coffee.changeToPortrait
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.NoActivityResumedException
+import com.sserra.coffee.*
+import org.junit.Assert
 
 abstract class Page {
     fun changeToLandscape(): Page = apply { changeToLandscape }
     fun changeToPortrait(): Page = apply { changeToPortrait }
+    fun navigateBack(): Page = apply { Espresso.pressBack() }
+    fun navigateBackShouldWork() : Page = apply {
+        // This is how Google does it
+        // https://android.googlesource.com/platform/frameworks/testing/+/61a929bd4642b9042bfb05b85340c1761ab90733/espresso/espresso-lib-tests/src/androidTest/java/com/google/android/apps/common/testing/ui/espresso/action/KeyEventActionIntegrationTest.java
+        try {
+            navigateBack()
+            Assert.fail("Should have thrown NoActivityResumedException")
+        } catch (expected: NoActivityResumedException) {
+        }
+    }
 }
--- coffee/src/main/java/com/sserra/coffee/coffeepages/PageWithToolbar.kt
+++ coffee/src/main/java/com/sserra/coffee/coffeepages/PageWithToolbar.kt
@@ -1,10 +1,17 @@
 package com.sserra.coffee.coffeepages
 
+import android.annotation.SuppressLint
+import com.sserra.coffee.R
+import com.sserra.coffee.click
 import com.sserra.coffee.coffeviews.CoffeeView
 import com.sserra.coffee.coffeviews.ToolbarCoffeeView
+import com.sserra.coffee.onViewWithDescription
 
 abstract class PageWithToolbar(private val toolbarId: Int, private val appBarId: Int) : Page() {
     fun appBar(block: CoffeeView.() -> Unit = {}): CoffeeView = CoffeeView(appBarId, block)
     fun toolbar(block: ToolbarCoffeeView.() -> Unit = {}): ToolbarCoffeeView =
             ToolbarCoffeeView(toolbarId, block)
+
+    @SuppressLint("PrivateResource")
+    fun navigateUp(): Page = apply { onViewWithDescription(R.string.abc_action_bar_up_description).click() }
 }
